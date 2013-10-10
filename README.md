d4404c8770097ba9746e110461cce143139f15fa
Bill keller <bill@billkeller.name>
2013-10-10 16:57:53 -0400

    sass dir renamed scss, udpates readme

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Requirements:
 1) gem install bootstrap-sass
 
 2) from root dir run 'bower install'
+
+3) from root dir run 'compass watch css/scss/style.css'
--- config.rb
+++ config.rb
@@ -23,7 +23,7 @@ require 'susy'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "css/sass"
+sass_dir = "css/scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
