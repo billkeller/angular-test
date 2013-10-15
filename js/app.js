0e3d458b1ff2d8112f2889d2ec8f4b47d7441179
Bill keller <bill@billkeller.name>
2013-10-15 17:57:15 -0400

    latest angular, testing tabs

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ function AlertDemoCtrl($scope) {
 
 }
 
-function TabsDemoCtrl($scope) {
+var TabsDemoCtrl = function ($scope) {
   $scope.tabs = [
     { title:"Dynamic Title 1", content:"Dynamic content 1" },
     { title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
